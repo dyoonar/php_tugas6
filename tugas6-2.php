24df098fca695ce0bfd9dccb7f71dd6f928d2e71
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-08 10:43:44 +0700

    Add files via upload

---
+++ tugas6-2.php
@@ -0,0 +1,18 @@
+<?php
+$a=6;
+$b=5;
+$c= 15000;
+$d=2000;
+$e= ($b*$a*$c)+($a*$d);
+
+
+echo "<br>=====================================<br>";
+echo "Harga Jambu = Rp ".$c."<br>";
+echo "Harga Kardus = Rp ".$b."<br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp ".$e."<br>";
+echo "<br>=====================================<br>";
+// echo "Harga Jambu = Rp ".$c."<br>";
+// echo "Harga Kardus = Rp ".$b."<br>";
+// echo "Total Penjualan (Dus dan Jambu) = Rp?<br>";
+// echo "<br>=====================================<br>";
+?>
